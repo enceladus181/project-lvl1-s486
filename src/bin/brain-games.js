aec26ce538a7f0b4ab4fccb338904985c997411a
Magzhan Sydykov <justakamas@hotmail.com>
2019-04-11 09:27:06 +0600

    assertion failed error

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import sqrOfNum from '..';
+import name from '..';
 console.log('Welcome to the Brain Games!');
-sqrOfNum();
+console.log('Hello ' + name + '!');
--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-//import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-//const name = readlineSync.question('May I have your name? ');
-//console.log('Hello, ' + name + '!');
-
-const num = 25;
-const sqrOf = (n) => n * n;
-const sqrOfNum = sqrOf(num);
-export default sqrOfNum;
+const name = readlineSync.question('May I have your name? ');
+export default name;
