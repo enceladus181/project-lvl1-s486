4ba55ce20134bdc90cfc9733ca14fe9b76270a90
Magzhan Sydykov <justakamas@hotmail.com>
2019-04-12 23:46:44 +0600

    deleted plugin-proposal-pipeline-operator dependency

--- babel.config.js
+++ babel.config.js
@@ -8,13 +8,5 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    [
-      '@babel/plugin-proposal-pipeline-operator',
-      {
-        proposal: 'minimal',
-      },
-    ],
-  ],
 };
 
