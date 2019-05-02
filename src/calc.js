2fee5f3f3f2bfc3914ae2f623d273d241cc795ac
Magzhan Sydykov <justakamas@hotmail.com>
2019-05-02 12:55:57 +0600

    experimental commit after reinstalling Windows OS

--- src/calc.js
+++ src/calc.js
@@ -11,7 +11,43 @@ const expressions = [operand1 - operand2,
                        operand1 + operand2,
                        operand1 * operand2];
 
+<<<<<<< Updated upstream
 const question = () => `${operand1} ${operators[operator]} ${operand2}`;
 const rightAnswer = () => expressions[operator];
+||||||| merged common ancestors
+const calcGame = () => {
+  const generateCalcGame = () => {
+    const operand1 = getRandomNumber();
+    const operand2 = getRandomNumber();
+    const operators = ['-', '+', '*'];
+    const getRandomOperator = (arr) => Math.floor(Math.random() * arr.length);
+    const operator = getRandomOperator(operators);
+    const expressions = [operand1 - operand2,
+                           operand1 + operand2,
+                           operand1 * operand2];
+    const getQuestion = () => `${operand1} ${operators[operator]} ${operand2}`;
+    const getRightAnswer = () => expressions[operator];
+    return { getQuestion, getRightAnswer };
+  };
+  return engine(taskExplanationMessage, generateCalcGame);
+}
+=======
+const calcGame = () => {
+  const generateCalcGame = () => {
+    const operand1 = getRandomNumber();
+    const operand2 = getRandomNumber();
+    const operators = ['-', '+', '*'];
+    const getRandomOperator = (arr) => Math.floor(Math.random() * arr.length);
+    const operator = getRandomOperator(operators);
+    const expressions = [operand1 - operand2,
+                           operand1 + operand2,
+                           operand1 * operand2];
+    const question = `${operand1} ${operators[operator]} ${operand2}`;
+    const rightAnswer = expressions[operator];
+    return { question, rightAnswer };
+  };
+  return engine(taskExplanationMessage, { generateCalcGame });
+};
+>>>>>>> Stashed changes
 
 export default engine(taskExplanationMessage, question, rightAnswer);
--- src/index.js
+++ src/index.js
@@ -9,20 +9,43 @@ const engine = (taskExplanationMessage, question, rightAnswer) => {
 	console.log(taskExplanationMessage);
   const playerName = getPlayerName();
   const numOfTries = 3;
+<<<<<<< Updated upstream
   let i = 1;
   for (; i <= numOfTries; i += 1) {
     console.log(`Question: ${question()}`);
+||||||| merged common ancestors
+  let i = 1;
+  for (; i <= numOfTries; i += 1) {
+		const question = gameData.getQuestion();
+    console.log(`Question: ${question}`);
+=======
+  let counter = 1;
+  
+  for (; counter <= numOfTries; counter += 1) {
+		const { question, rightAnswer } = gameData.generateCalcGame();
+    console.log(`Question: ${question}`);
+>>>>>>> Stashed changes
     const playerAnswer = getPlayerAnswer();
+<<<<<<< Updated upstream
     
     if (playerAnswer == rightAnswer()) {
         console.log('Correct!');
+||||||| merged common ancestors
+    if (playerAnswer == rightAnswer) {
+        console.log('Correct!');
+=======
+    
+    if (playerAnswer == rightAnswer) {
+      console.log('Correct!');
+>>>>>>> Stashed changes
     } else {
-        console.log(`'${playerAnswer}' is wrong answer ;(, Correct answer was '${rightAnswer}'.`);
-        console.log(`Let's try again, ${playerName}!`);
+      console.log(`'${playerAnswer}' is wrong answer ;(, Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${playerName}!`);
+      break;
     }
-  }
-  if (i === 3) {
-        console.log(`Congratulations, ${playerName}!`);	
+    if (counter === 3) {
+    	console.log(`Congratulations, ${playerName}!`);	
+		}
   }
 };
 
